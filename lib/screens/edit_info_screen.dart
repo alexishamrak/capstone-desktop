19affc7d7228263e011529a56683e3321a7b2b28
Julien Campbell <jrc101598@gmail.com>
2023-01-27 13:24:10 -0700

    Remove edit info page

--- lib/screens/edit_info_screen.dart
+++ lib/screens/edit_info_screen.dart
@@ -14,7 +14,26 @@ class _EditInfoScreenState extends State<EditInfoScreen> {
           appBar: const NavBar(
             color: Colors.blue,
         ), //NavBar
-          //body: 
+          body: Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter a search term',
+                        ),
+                      ),
+                    ], //<Widget>[]
+                  ), //Row
+                ), //Padding
+              ],
+            ),
+          ),//Padding
       ), //Scaffold
     ); //SafeArea
   }//build
